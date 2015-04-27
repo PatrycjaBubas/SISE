7715e5d30fb2eb98100831d375cd1df684b3b133
Patrycja <patrycja.bubas@gmail.com>
2015-04-27 17:26:40 +0200

    działające

--- Z1_pietnastka/Z1_pietnastka/main.cpp
+++ Z1_pietnastka/Z1_pietnastka/main.cpp
@@ -15,6 +15,9 @@
 
 using namespace std;
 
+int wiersze, kolumny = 0;
+
+
 bool otworz_do_odczytu(ifstream &plik, char nazwa_pliku[])
 {
 	plik.open(nazwa_pliku, ios_base::in);
@@ -114,6 +117,8 @@ int _tmain(int argc, char* argv[])
 	//algorytm przeszukiwania
 	char przeszukiwanie;
 
+	int wybor;
+
 	przeszukiwanie = *argv[1];
 
 	cout << "\nPrzeszukiwanie: " << przeszukiwanie << endl;
@@ -123,16 +128,120 @@ int _tmain(int argc, char* argv[])
 	strategia[2] = *argv[4];
 	strategia[3] = *argv[5];
 
-	cout << "\nStrategia:" << endl;
+	cout << "\nStrategia: ";
 
 	for (int i = 0; i < 4; i++)
 	{
-		cout<<strategia[i]<<endl;
+		cout<<strategia[i]<<" ";
 	}
 
 
-	cout << endl;
+	cout << "\n\nPodaj w jaki sposob chcesz wczytac poczatkowy uklad planszy. Wcisnij:"
+		"\n1 - jesli chcesz wczytac z pliku"
+		"\n2 - jesli chcesz podac ja w konsoli" << endl;
+	cout << "\nTwoj wybor: ";
+	cin >> wybor;
+	
 
+	switch (wybor)
+	{
+	case 1:
+		//odczyt pliku
+		if (otworz_do_odczytu(plik_we, "dane.txt") == false) return 0;
+		lista_pol = odczytaj(plik_we);
+		plik_we.close();
+
+		cout << "Wczytano plansze: " << endl << endl;
+		uloz_plansze(lista_pol);
+
+		break;
+	case 2:
+		int wymiary[2];
+		int w, k;
+
+		cout << "\nPodaj wymiary tablicy:" << endl;
+		cin >> w >> k;
+		//cout << "W: " << w << " K: " << k << endl;
+
+		wiersze = w;
+		kolumny = k;
+
+		//utworzenie tablicy dwuwymiarowej odpowiadajacej za wyglad planszy
+		int** plansza = new int*[wiersze];
+		for (int i = 0; i < wiersze; ++i)
+		{
+			plansza[i] = new int[kolumny];
+		}
+
+		cout << "\nPodaj stan poczatkowy ukladanki:" << endl;
+
+		for (int i = 0; i < wiersze; i++)
+		{
+			for (int j = 0; j < kolumny; j++)
+			{
+				cin >> plansza[i][j];
+			}
+		}
+
+		cout << "Wczytana plansza:" << endl;
+		for (int i = 0; i < wiersze; i++)
+		{
+			for (int j = 0; j < kolumny; j++)
+			{
+				cout << plansza[i][j];
+			}
+			cout << endl;
+		}
+
+		for (int i = 0; i < wiersze; i++)
+		{
+			for (int j = 0; j < kolumny; j++)
+			{
+				lista_pol.push_back(plansza[i][j]);
+			}
+		}
+
+
+		cout << "Wczytany wektor:" << endl;
+		for (int i = 0; i < wiersze*kolumny; i++)
+		{
+			cout << lista_pol[i] <<endl;
+		}
+
+		break;
+	}
+
+
+	lista_pol.erase(lista_pol.begin(), lista_pol.begin() + 2);
+
+
+	if (przeszukiwanie == 'b' || przeszukiwanie == 'B')
+	{
+		//obiektBFS
+		BFS obiektBFS = BFS(wiersze, kolumny);
+		obiektBFS.stworzPoprawnyStan();
+		obiektBFS.listaStanow.push_back(lista_pol);
+		obiektBFS.szukajBFS(strategia, lista_pol);
+	}
+	else if (przeszukiwanie == 'd' || przeszukiwanie == 'D')
+	{
+		//obiekt DFS
+		vector<char> strategiaDFS;
+		for (int x = 0; x<4; x++)
+		{
+			strategiaDFS.push_back(strategia[x]);
+		}
+		DFS obiektDFS = DFS(wiersze, kolumny);
+		obiektDFS.stworzPoprawnyStan();
+		obiektDFS.szukajDFS(strategiaDFS, lista_pol);
+	}
+	else
+	{
+		cout << "Nie ma takiego przeszukiwania!" << endl;
+	}
+
+	
+	cout << endl;
 
 	system("pause");
 	return 0;
