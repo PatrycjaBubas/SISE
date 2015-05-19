0740512d33459b52c8d8ded86bb5fceb1148eb6e
Patrycja <patrycja.bubas@gmail.com>
2015-05-19 17:02:47 +0200

    Z2

---
+++ Z2_Bubas_Woźnicka/Zad_Iris/src/main/java/Zad_Iris/Zad_Iris/App.java
@@ -0,0 +1,39 @@
+package Zad_Iris.Zad_Iris;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Iris;
+import Model.LearnedIris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    	public static void main( String[] args ) throws IOException{
+        	IrisLoad loader = new IrisLoad();
+    		List<Iris> knownIrisList = new ArrayList<Iris>();
+    		List<Iris> testIrisList = new ArrayList<Iris>();
+    		List<LearnedIris> irisWithDistances = new ArrayList<LearnedIris>();
+    		try {
+    			// załadowanie plików wejściowych i wczytanie ich jako obiekty Iris
+    			// do list learnList i testList
+    			loader.read("Learn.csv", "Test.csv");
+    			for(Iris iris : loader.learnList){
+    				knownIrisList.add(iris);
+    			}
+    			for(Iris iris : loader.qualifyList){
+    				testIrisList.add(iris);
+    			}
+    			
+    			System.out.println(loader.learnList.size());
+
+    		} catch (FileNotFoundException fnfE) {
+    			System.out.println(fnfE);
+    		}
+        }
+    }
