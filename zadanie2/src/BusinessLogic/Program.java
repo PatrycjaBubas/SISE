f52d6e7ed517c5b315fef07ac857ca86420bad24
naravien <woznicka.daria@gmail.com>
2015-05-26 00:12:08 +0200

    usunieto metode classify, dodano klase LearnedIris ittp itd.

--- zadanie2/src/BusinessLogic/Program.java
+++ zadanie2/src/BusinessLogic/Program.java
@@ -4,25 +4,28 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.common.collect.*;
 
 import Model.Iris;
+import Model.LearnedIris;
 
 public class Program {
 
 	public static void main(String[] args) throws IOException {
 
+		// deklaracja zmiennych i list
 		String fileIn = null, fileOut = null, fileTest = null, m = null;
-		int state = -1, k = -1, c = -1, rok = -1;
+		Integer state = -1, k = -1, c = -1;
 		Boolean r = false;
 		IrisLoad loader = new IrisLoad();
 		List<Iris> knownIrisList = new ArrayList<Iris>();
 		List<Iris> testIrisList = new ArrayList<Iris>();
-		ListMultimap<Float, List<Iris>> irisWithDistances = ArrayListMultimap
-				.create();
+		List<LearnedIris> learnedIrises = new ArrayList<LearnedIris>();
 
+		// pobranie argumentow uruchomienia
 		for (int i = 0; i < args.length; ++i) {
 			if (state == 6) {
 				c = Integer.valueOf(args[i]);
@@ -80,24 +83,61 @@ public class Program {
 			System.out.println(fnfE);
 		}
 
-		if (r == true) {
+		if (r == false) {
+			for (Iris iris : testIrisList) {
+				int counterSetosa = 0;
+				int counterVersicolor = 0;
+				int counterVirginica = 0;
+				boolean conflict = false;
+				for (Iris oldIris : knownIrisList) {
+					LearnedIris newLearnedIris = new LearnedIris();
+					newLearnedIris.setNewIris(iris);
+					newLearnedIris.setOldIris(oldIris);
+					newLearnedIris.setDistance(getDistance(oldIris, iris, m));
+					learnedIrises.add(newLearnedIris);
+				}
+				Collections.sort(learnedIrises);
+				for (int i = 0; i < k; i++) {
+					String type = learnedIrises.get(i).getOldIris().getType();
+					switch (type) {
+					case "Iris-setosa":
+						counterSetosa++;
+						break;
+					case "Iris-versicolor":
+						counterVersicolor++;
+						break;
+					case "Iris-virginica":
+						counterVirginica++;
+						break;
+					}
+				}
 
+				// sprawdzenie, czy nie nastal konflikt
+				if ((counterSetosa == counterVersicolor && counterSetosa != 0)
+						|| (counterSetosa == counterVirginica && counterSetosa != 0)
+						|| (counterVersicolor == counterVirginica && counterVersicolor != 0)) {
+
+					// rozwi¹¿ konflikt
+				}
+
+			}
 		} else {
 
 		}
 
-		// for (Iris test : testIrisList) {
-		// for (Iris known : knownIrisList) {
-		// System.out.println(getDistance(known, test, m));
-		// List<Iris> irisList = new ArrayList<>();
-		// irisList.add(known);
-		// irisList.add(test);
-		// irisWithDistances.put(getDistance(known, test, m), irisList);
-		// }
-		// }
-
 	}
 
+	/**
+	 * Metoda obliczajaca dystans miedzy dwoma obiektami Iris. jesli parametrem
+	 * metryki m jest 'CITY', wtedy dystans liczony jest sposobem Manhattan.
+	 * Jeœli parametr m jest inny, wtedy dystans liczony jest w sposób
+	 * euklidesowski (najkrótsza droga).
+	 *
+	 * @param oldIris
+	 * @param newIris
+	 * @param m
+	 * @return float dystans
+	 */
 	public static float getDistance(Iris oldIris, Iris newIris, String m) {
 
 		float sepalLengthDistance, sepalWidthDistance, sepalDistance, petalLengthDistance, sepalPetalDistance, petalWidthDistance, finalDistance;
@@ -131,58 +171,6 @@ public class Program {
 			finalDistance = sepalPetalDistance + petalWidthDistance;
 		}
 		return finalDistance;
-	}
-
-	public int classify(Iris irisToclassify, List<Iris> knownIrises, int k,
-			String m) {
-		if (k % 2 == 0) {
-			k += 1;
-		}
-		Multimap<Float, Iris> irisWithDistance = ArrayListMultimap.create();
-		List<String> listOfClosestIrises = new ArrayList<String>();
-		int counterSetosa = 0;
-		int counterVersicolor = 0;
-		int counterVirginica = 0;
-
-		for (Iris ir : knownIrises) {
-			irisWithDistance.put(getDistance(ir, irisToclassify, m), ir);
-		}
-		int i = 0;
-		for (Iris iris : irisWithDistance.values()) {
-			if (i < k) {
-				listOfClosestIrises.add(iris.getType());
-				i += 1;
-			} else {
-				break;
-			}
-		}
-
-		for (String iter : listOfClosestIrises) {
-			switch (iter) {
-			case "Iris-setosa":
-				counterSetosa++;
-				break;
-			case "Iris-versicolor":
-				counterVersicolor++;
-				break;
-			case "Iris-virginica":
-				counterVirginica++;
-				break;
-			}
-		}
-
-		if (counterSetosa > counterVersicolor
-				&& counterSetosa > counterVirginica) {
-			return 0;
-		} else if (counterVersicolor > counterSetosa
-				&& counterVersicolor > counterVirginica) {
-			return 1;
-		} else if (counterVirginica > counterVersicolor
-				&& counterVirginica > counterSetosa) {
-			return 2;
-		} else
-			return -1;
-
 	}
 
 }
